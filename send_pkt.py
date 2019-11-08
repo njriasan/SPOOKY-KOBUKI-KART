47e131ce0452f795651a55d538dc9f244a63ff21
njriasan <njriasanovsky@berkeley.edu>
2019-11-07 21:18:55 -0800

    downgraded the send_pkt file to a client that prints

--- send_pkt.py
+++ send_pkt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import struct
 import time
-import keyboard
 from getpass import getpass
 from bluepy.btle import Peripheral, DefaultDelegate
 import argparse
@@ -9,10 +8,12 @@ import socket
 
 parser = argparse.ArgumentParser(description='Print advertisement data from a BLE device')
 parser.add_argument('addr', metavar='A', type=str, help='Address of the form XX:XX:XX:XX:XX:XX')
+parser.add_argument('server_port', metavar='P', type=int, help='Port to connect to bluetooth endpoint')
 args = parser.parse_args()
 addr = args.addr.lower()
-if len(addr) != 17:
-    raise ValueError("Invalid address supplied")
+server_port = args.server_port
+#if len(addr) != 17:
+#    raise ValueError("Invalid address supplied")
 
 SERVICE_UUID = "4607eda0-f65e-4d59-a9ff-84420d87a4ca"
 CHAR_UUIDS = [
@@ -24,46 +25,47 @@ CHAR_UUIDS = [
 
 class RobotController():
 
-    def __init__(self, address):
+    def __init__(self, address, server_port):
 
         # robot refers to buckler, our peripheral
-        self.robot = Peripheral(addr)
+        #self.robot = Peripheral(addr)
         print("connected")
                         
         # keep state for keypresses
         # these are in order of "byte" that gets updated from 0 -> 8
         # controller is oriented sideways (we used left contoller for testing)
-        self.pressed = {"right": False, "down-right": False, "decelerate": False, "down-left": False,
-                        "left": False, "up-left": False, "accelerate": False, "up-right": False, 
-                        "stationary": False}
+        #self.pressed = {"right": False, "down-right": False, "decelerate": False, "down-left": False,
+        #                "left": False, "up-left": False, "accelerate": False, "up-right": False, 
+        #                "stationary": False}
 
         # get service from robot
-        self.sv = self.robot.getServiceByUUID(SERVICE_UUID)
+        #self.sv = self.robot.getServiceByUUID(SERVICE_UUID)
 
-        self.characteristics = {}
-        self.characteristics["right"] = self.sv.getCharacteristics(CHAR_UUIDS[2])[0]
-        self.characteristics["down-right"] = self.sv.getCharacteristics(CHAR_UUIDS[2])[0]
-        self.characteristics["up-right"] = self.sv.getCharacteristics(CHAR_UUIDS[2])[0]
+        #self.characteristics = {}
+        #self.characteristics["right"] = self.sv.getCharacteristics(CHAR_UUIDS[2])[0]
+        #self.characteristics["down-right"] = self.sv.getCharacteristics(CHAR_UUIDS[2])[0]
+        #self.characteristics["up-right"] = self.sv.getCharacteristics(CHAR_UUIDS[2])[0]
 
-        self.characteristics["left"] = self.sv.getCharacteristics(CHAR_UUIDS[3])[0]
-        self.characteristics["down-left"] = self.sv.getCharacteristics(CHAR_UUIDS[3])[0]
-        self.characteristics["up-left"] = self.sv.getCharacteristics(CHAR_UUIDS[3])[0]
+        #self.characteristics["left"] = self.sv.getCharacteristics(CHAR_UUIDS[3])[0]
+        #self.characteristics["down-left"] = self.sv.getCharacteristics(CHAR_UUIDS[3])[0]
+        #self.characteristics["up-left"] = self.sv.getCharacteristics(CHAR_UUIDS[3])[0]
 
         # not including up or down since they shouldn't mean anything wrt direction
 
         # get characteristic handles from service/robot
-        self.characteristics["accelerate"] = self.sv.getCharacteristics(CHAR_UUIDS[0])[0]
+        #self.characteristics["accelerate"] = self.sv.getCharacteristics(CHAR_UUIDS[0])[0]
 
-        self.characteristics["decelerate"] = self.sv.getCharacteristics(CHAR_UUIDS[1])[0]
+        #self.characteristics["decelerate"] = self.sv.getCharacteristics(CHAR_UUIDS[1])[0]
 
         # PUT SOCKET LISTENING CODE HERE
         # will call on_pkt_receive
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect('localhost', 10000)
+        self.sock.connect(('localhost', server_port))
 
         while True:
             pkt = self.sock.recv(12)
-            self.on_pkt_receive(pkt)
+            #self.on_pkt_receive(pkt)
+            print (pkt)
 
     def on_pkt_receive(self, pkt):
 
@@ -89,14 +91,14 @@ class RobotController():
         prev_pressed = self.pressed.copy()
 
         # set each button to its corresponding pkt bit
-        self.pressed["up-right"] = (bool) pkt & (0x1 << 1)
-        self.pressed["right"] = (bool) pkt & (0x1 << 1)
-        self.pressed["down-right"] = (bool) pkt & (0x1 << 1)
-        self.pressed["down-left"] = (bool) pkt & (0x1 << 1)
-        self.pressed["left"] = (bool) pkt & (0x1 << 1)
-        self.pressed["up-left"] = (bool) pkt & (0x1 << 1)
-        self.pressed["accelerate"] = (bool) pkt & (0x1 << 1)
-        self.pressed["decelerate"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["up-right"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["right"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["down-right"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["down-left"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["left"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["up-left"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["accelerate"] = (bool) pkt & (0x1 << 1)
+        #self.pressed["decelerate"] = (bool) pkt & (0x1 << 1)
 
         # write to characteristic if a button is pressed (True value) 
         # or released (False value)
@@ -109,5 +111,5 @@ class RobotController():
     def __exit__(self, exc_type, exc_value, traceback):
         self.robot.disconnect()
 
-with RobotController(addr) as robot:
+with RobotController(addr, server_port) as robot:
     getpass('Use arrow keys to control robot')
