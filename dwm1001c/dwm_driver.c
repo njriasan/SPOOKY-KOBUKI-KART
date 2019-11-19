d1a30a239b63c231e1c16149c55a71b4832e7b29
njriasan <njriasanovsky@berkeley.edu>
2019-11-18 17:25:54 -0800

    updated dwm_driver with proper shifting

--- dwm1001c/dwm_driver.c
+++ dwm1001c/dwm_driver.c
@@ -7,14 +7,23 @@
 #include "dwm_driver.h"
 
 static nrf_drv_spi_t* spi;
+static void update_message(uint8_t *msg, size_t msg_len);
+
+static void update_message(uint8_t *msg, size_t msg_len) {
+  for (size_t i = 0; i < msg_len; i++) {
+    msg[i] = msg[i] << 1;
+  }
+}
+
 
 uint8_t* dwm_init(nrf_drv_spi_t* s) {
   spi = s;
   uint8_t data[4];
-  data[0] = 0x0A;
+  data[0] = 0x05;
   data[1] = 0x02;
   data[2] = 0xD6;
   data[3] = 0x00;
+  update_message(data, 4);
   ret_code_t err_code = nrf_drv_spi_transfer(spi, data, 4, NULL, 0);
   APP_ERROR_CHECK(err_code);
   if (err_code != NRF_SUCCESS) {
@@ -23,7 +32,6 @@ uint8_t* dwm_init(nrf_drv_spi_t* s) {
   uint8_t size_num[2];
   err_code = nrf_drv_spi_transfer(spi, NULL, 0, size_num, 2);
   while (size_num[0] == 0x00) {
-    printf("%x %x\n", size_num[0], size_num[1]);
     APP_ERROR_CHECK(err_code);
     if (err_code != NRF_SUCCESS) {
       return NULL;
@@ -45,8 +53,10 @@ uint8_t* dwm_init(nrf_drv_spi_t* s) {
 uint8_t *dwm_read_rate(nrf_drv_spi_t *s) {
   spi = s;
   uint8_t data[2];
-  data[0] = 0x08;
+  data[0] = 0x04;
   data[1] = 0x00;
+  printf("input commands: %x %x\n", data[0], data[1]);
+  update_message(data, 2);
   ret_code_t err_code = nrf_drv_spi_transfer(spi, data, 2, NULL, 0);
   APP_ERROR_CHECK(err_code);
   if (err_code != NRF_SUCCESS) {
@@ -55,7 +65,48 @@ uint8_t *dwm_read_rate(nrf_drv_spi_t *s) {
   uint8_t size_num[2];
   err_code = nrf_drv_spi_transfer(spi, NULL, 0, size_num, 2);
   while (size_num[0] == 0x00) {
-    printf("%x %x\n", size_num[0], size_num[1]);
+    APP_ERROR_CHECK(err_code);
+    if (err_code != NRF_SUCCESS) {
+      return NULL;
+    }
+    nrf_delay_ms(10);
+    err_code = nrf_drv_spi_transfer(spi, NULL, 0, size_num, 2);
+  }
+  printf("%x %x\n", size_num[0], size_num[1]);
+  uint8_t* readData = (uint8_t *)malloc(sizeof(uint8_t)*size_num[0]);
+  err_code = nrf_drv_spi_transfer(spi, NULL, 0, readData, size_num[0]);
+  APP_ERROR_CHECK(err_code);
+  if (err_code != NRF_SUCCESS) {
+    return NULL;
+  }
+  int i = 0;
+  while (i < size_num[0]) {
+    printf("%x ", readData[i++]);
+  }
+  printf("\n");
+  return readData;
+
+}
+
+uint8_t *dwm_write_rate(nrf_drv_spi_t *s) {
+  spi = s;
+  uint8_t data[6];
+  data[0] = 0x03;
+  data[1] = 0x04;
+  data[2] = 0x0B;
+  data[3] = 0x00;
+  data[4] = 0x32;
+  data[5] = 0x00;
+  printf("input commands: %x %x\n", data[0], data[1]);
+  update_message(data, 6);
+  ret_code_t err_code = nrf_drv_spi_transfer(spi, data, 6, NULL, 0);
+  APP_ERROR_CHECK(err_code);
+  if (err_code != NRF_SUCCESS) {
+    return NULL;  
+  }
+  uint8_t size_num[2];
+  err_code = nrf_drv_spi_transfer(spi, NULL, 0, size_num, 2);
+  while (size_num[0] == 0x00) {
     APP_ERROR_CHECK(err_code);
     if (err_code != NRF_SUCCESS) {
       return NULL;
