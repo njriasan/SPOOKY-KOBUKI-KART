ea627507b7331dd41cc250b3de4631c41e0e43d9
GrantMeAWish <grant.wang5@gmail.com>
2019-11-25 15:15:41 -0800

    fixed some spacing issues

--- led/main.c
+++ led/main.c
@@ -28,7 +28,7 @@ void pwm_ready_callback(uint32_t pwm_id) {
 int main(void) {
 
     /* Configure pwm channel period and pin number */
-	app_pwm_config_t pwm1_cfg = APP_PWM_DEFAULT_CONFIG_1CH (5000L, 27); 
+    app_pwm_config_t pwm1_cfg = APP_PWM_DEFAULT_CONFIG_1CH(5000.0, 27); 
 
     /* Switch the polarity of the second channel. */
     pwm1_cfg.pin_polarity[0] = APP_PWM_POLARITY_ACTIVE_HIGH;
@@ -38,11 +38,11 @@ int main(void) {
     err_code = app_pwm_init(&PWM1, &pwm1_cfg, pwm_ready_callback); 
     APP_ERROR_CHECK(err_code);
     
-	app_pwm_enable(&PWM1); // Enable pwm
+    app_pwm_enable(&PWM1); // Enable pwm
     printf("%s\n", "Done enabling pwm");
 
     /* Set pwm duty cycle */
-	app_pwm_channel_duty_set(&PWM1, 0, 80);
+    app_pwm_channel_duty_set(&PWM1, 0, 80);
     
 
     while (true) {
