2a905a76452a3989faa07a473ef7eb9931737d52
GrantMeAWish <grant.wang5@gmail.com>
2019-12-09 18:36:52 -0800

    got led flashing to work

--- led/pwm.h
+++ led/pwm.h
@@ -6,9 +6,9 @@
 #include "nrf_drv_pwm.h"
 
 void pwm_init();
-void light_green();
+void light_green(uint32_t num_leds);
 void light_blue();
 void light_red();
-void clear_lights();
+void clear_lights(uint32_t num_leds);
 
 #endif
