318b10169164b099f6071f6ef62d5f48d20b55a9
GrantMeAWish <grant.wang5@gmail.com>
2019-12-09 20:59:24 -0800

    led flash code modularized better in led folder, needs to be integrated with buckler

--- led/pwm.h
+++ led/pwm.h
@@ -6,9 +6,6 @@
 #include "nrf_drv_pwm.h"
 
 void pwm_init();
-void light_green(uint32_t num_leds);
-void light_blue();
-void light_red();
-void clear_lights(uint32_t num_leds);
+void lightup_led(uint32_t num_leds, uint32_t color_id);
 
 #endif
